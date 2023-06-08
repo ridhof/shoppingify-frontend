96b54b03d133d35fab013e0adf6684627ce24dcb
Dian Rahmaji <dianrahmaji@gmail.com>
2023-06-08 09:32:02 +0700

    chore: setup tailwind color and font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#F9A109",
+        secondary: "#FFF0DE",
+        gray: {
+          200: "#F2F2F2",
+          300: "#BDBDBD",
+          400: "#C1C1C4",
+          500: "#828282",
+          600: "#34333A",
+          700: "#454545",
+        },
+        "light-blue": "#56CCF2",
+        tomato: "#EB5757",
+        maroon: "#80485B",
+      },
+      fontFamily: {
+        sans: ["var(--font-quicksand)"],
       },
     },
   },
   plugins: [],
-}
+};
