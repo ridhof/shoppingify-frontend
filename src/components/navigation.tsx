e3ff79cc6486883980e672165c12329f39ca73b4
Dian Rahmaji <dianrahmaji@gmail.com>
2023-06-09 14:22:42 +0700

    feat: add Logo and ShoppingCart in Navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-// import { FormatListBulleted, Replay, InsertChart } from "~/components/icons";
 
 import FormatListBulleted from "@material-design-icons/svg/filled/format_list_bulleted.svg";
 import Replay from "@material-design-icons/svg/filled/replay.svg";
 import InsertChart from "@material-design-icons/svg/outlined/insert_chart_outlined.svg";
+import ShoppingCart from "@material-design-icons/svg/outlined/shopping_cart.svg";
 
 import Tooltip from "~/components/ui/tooltip";
 
@@ -15,40 +16,55 @@ function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed flex h-full flex-col items-center justify-center gap-11">
-      <Tooltip label="items" position="right">
-        <Link
-          href="/"
-          className={clsx("relative px-6 py-3", {
-            " before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
-              pathname === "/",
-          })}
-        >
-          <FormatListBulleted className="fill-gray-700" />
-        </Link>
-      </Tooltip>
-      <Tooltip label="history" position="right">
-        <Link
-          href="/history"
-          className={clsx("relative px-6 py-3", {
-            "before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
-              pathname === "/history",
-          })}
-        >
-          <Replay className="fill-gray-700" />
-        </Link>
-      </Tooltip>
-      <Tooltip label="statistics" position="right">
-        <Link
-          href="/statistics"
-          className={clsx("relative px-6 py-3", {
-            "before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
-              pathname === "/statistics",
-          })}
-        >
-          <InsertChart className="fill-gray-700" />
-        </Link>
-      </Tooltip>
+    <nav className="fixed flex h-full flex-col justify-between py-10">
+      <Image
+        className="mx-auto cursor-pointer"
+        src="/images/logo.svg"
+        alt="Shoppingify Logo"
+        width={40}
+        height={40}
+      />
+      <div className="flex flex-col gap-11">
+        <Tooltip label="items" position="right">
+          <Link
+            href="/"
+            className={clsx("relative px-6 py-3", {
+              " before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
+                pathname === "/",
+            })}
+          >
+            <FormatListBulleted className="fill-gray-700" />
+          </Link>
+        </Tooltip>
+        <Tooltip label="history" position="right">
+          <Link
+            href="/history"
+            className={clsx("relative px-6 py-3", {
+              "before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
+                pathname === "/history",
+            })}
+          >
+            <Replay className="fill-gray-700" />
+          </Link>
+        </Tooltip>
+        <Tooltip label="statistics" position="right">
+          <Link
+            href="/statistics"
+            className={clsx("relative px-6 py-3", {
+              "before:absolute before:left-0 before:top-0 before:h-full before:w-[6px] before:rounded-r-lg before:bg-primary":
+                pathname === "/statistics",
+            })}
+          >
+            <InsertChart className="fill-gray-700" />
+          </Link>
+        </Tooltip>
+      </div>
+      <button className="relative mx-auto h-10 w-10 rounded-full bg-primary">
+        <ShoppingCart className="mx-auto fill-white" />
+        <span className="absolute right-0 top-0 -translate-y-1/4 translate-x-1/4 rounded bg-tomato px-2 py-0.5  text-xs text-white">
+          3
+        </span>
+      </button>
     </nav>
   );
 }
