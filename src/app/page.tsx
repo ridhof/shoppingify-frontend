64cdefe3836e8af10ae375b10bec49a8e26ec808
Dian Rahmaji <dianrahmaji@gmail.com>
2023-06-10 15:12:30 +0700

    feat: add ShoppingList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 import SearchBar from "~/components/search-bar";
+import ShoppingList from "~/components/shopping-list";
 
-export default function Items() {
+const shoppingLists = [
+  {
+    category: "Fruit and vegetables",
+    items: [
+      "Avocado",
+      "Banana",
+      "Bunch of carrots 5pcs",
+      "Chicken 1kg",
+      "Pre-cooked corn 450gr",
+      "Mandarin Nadorcott",
+      "Piele De Sapo Melon",
+      "Watermelon",
+    ],
+  },
+  {
+    category: "Meat and Fish",
+    items: [
+      "Chicken leg box",
+      "Chicken 1kg",
+      "Pork fillets 450kg",
+      "Salmon 1kg",
+    ],
+  },
+];
+
+export default function ShoppingLists() {
   return (
-    <main className="bg-gray-100 py-9 pl-44">
-      <header className="flex gap-16">
+    <main className="min-h-screen w-3/4 bg-gray-100 py-9 pl-44">
+      <header className=" mb-14 flex justify-between">
         <h1 className="text-2xl font-medium leading-8">
           <span className="font-bold text-primary">Shoppingify</span> allows you
           take your
@@ -12,6 +38,9 @@ export default function Items() {
         </h1>
         <SearchBar />
       </header>
+      {shoppingLists.map((list) => (
+        <ShoppingList key={list.category} {...list} />
+      ))}
     </main>
   );
 }
---
+++ src/components/shopping-list.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Add from "@material-design-icons/svg/filled/add.svg";
+
+interface ShoppingListProps {
+  category: string;
+  items: string[];
+}
+
+function ShoppingList({ category, items }: ShoppingListProps) {
+  return (
+    <section key={category} className="mb-12">
+      <h3 className="mb-4 text-lg font-medium">{category}</h3>
+      <div className="grid grid-cols-[repeat(auto-fill,_minmax(0,182px))] gap-5">
+        {items.map((item) => (
+          <div
+            key={item}
+            className="flex h-fit justify-between rounded-xl bg-white p-4 text-base font-medium shadow-[0_2px_12px_2px_rgba(0,0,0,0.05)]"
+          >
+            <span>{item}</span>
+            <Add className="fill-gray-400" />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default ShoppingList;
