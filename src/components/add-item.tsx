a2a444bcd439725214d852bdcb461e97630523e5
Dian Rahmaji <dianrahmaji@gmail.com>
2023-06-20 21:24:32 +0700

    feat: implement Downshift for Combobox

--- src/components/add-item.tsx
+++ src/components/add-item.tsx
@@ -1,45 +1,46 @@
 "use client";
 
+import Combobox from "./form/combobox";
+import Input from "./form/input";
+import TextArea from "./form/text-area";
+
+const categories = [
+  { id: 1, name: "Fruit and vegetables" },
+  { id: 2, name: "Meat and Fish" },
+  { id: 3, name: "Beverages" },
+];
+
 function AddItem() {
   return (
     <section className="absolute right-0 top-0 h-screen w-1/5">
       <div className="pl-12 pr-8 pt-11">
         <h1 className="mb-8 text-2xl font-medium">Add a new item</h1>
         <form>
-          <div className="mb-4">
-            <label htmlFor="name" className="mb-1 block text-sm font-medium">
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter a name"
-              className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="note" className="mb-1 block text-sm font-medium">
-              Note (optional)
-            </label>
-            <textarea
-              placeholder="Enter a note"
-              className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
-              rows={3}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="image" className="mb-1 block text-sm font-medium">
-              Image (optional)
-            </label>
-            <input
-              type="text"
-              id="image"
-              name="image"
-              placeholder="Enter a url"
-              className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
-            />
-          </div>
+          <Input
+            label="Name"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter a name"
+          />
+          <TextArea
+            label=" Note (optional)"
+            placeholder="Enter a note"
+            rows={3}
+          />
+          <Input
+            label="Image (optional)"
+            type="text"
+            id="image"
+            name="image"
+            placeholder="Enter a url"
+          />
+          <Combobox
+            placeholder="Enter a category"
+            label="Category"
+            itemToString={(item) => (item ? item.name : "")}
+            items={categories}
+          />
         </form>
       </div>
       <div className="absolute bottom-0 flex w-full items-center justify-center gap-5 pb-8">
---
+++ src/components/form/combobox.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import Downshift, { type DownshiftProps } from "downshift";
+import ChevronLeft from "@material-design-icons/svg/filled/chevron_right.svg";
+import Close from "@material-design-icons/svg/filled/close.svg";
+
+type ComboboxProps<T extends { id: number; name: string }> = {
+  label: string;
+  items: T[];
+} & React.HtmlHTMLAttributes<"input"> &
+  DownshiftProps<T>;
+
+function Combobox<T extends { id: number; name: string }>({
+  id,
+  label,
+  items,
+  placeholder,
+  ...props
+}: ComboboxProps<T>) {
+  return (
+    <Downshift {...props}>
+      {({ getInputProps, getItemProps, isOpen, selectedItem }) => (
+        <div>
+          <div className="relative w-full">
+            <label htmlFor={id} className="mb-1 block text-sm font-medium">
+              {label}
+            </label>
+            <input
+              {...getInputProps({ id, placeholder, isOpen })}
+              className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
+            />
+            {selectedItem ? (
+              <button className="absolute right-3 top-[50%]">
+                <Close fill="#828282" />
+              </button>
+            ) : (
+              <button className="absolute right-2 top-[50%]">
+                <ChevronLeft fill="#828282" className="h-14 w-14 rotate-90" />
+              </button>
+            )}
+          </div>
+          {isOpen && (
+            <ol className="mt-3 rounded-xl border-[1px] border-[#E0E0E0] bg-white p-2">
+              {items.map((item, index) => (
+                <li
+                  key={item.id}
+                  {...getItemProps({
+                    item,
+                    index,
+                    isSelected: selectedItem === item,
+                  })}
+                  className="cursor-pointer px-6 py-3 text-lg font-medium text-gray-500 hover:bg-gray-200 hover:text-gray-600"
+                >
+                  {item.name}
+                </li>
+              ))}
+            </ol>
+          )}
+        </div>
+      )}
+    </Downshift>
+  );
+}
+
+export default Combobox;
