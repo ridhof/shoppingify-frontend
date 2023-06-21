1104ba9b6d65fbfb2c6cd97e1d16a845ec81819e
Dian Rahmaji <dianrahmaji@gmail.com>
2023-06-21 16:02:03 +0700

    feat: integrate `Input` and `TextArea` with `react-hook-form`

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,18 +1,29 @@
-type InputProps = { label: string } & React.ComponentPropsWithoutRef<"input">;
+import { forwardRef } from "react";
 
-function Input({ id, label, ...props }: InputProps) {
-  return (
-    <div className="mb-4">
-      <label htmlFor={id} className="mb-1 block text-sm font-medium">
-        {label}
-      </label>
-      <input
-        id={id}
-        {...props}
-        className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
-      />
-    </div>
-  );
-}
+type InputProps = {
+  label: string;
+  error: string | undefined;
+} & React.ComponentPropsWithRef<"input">;
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, error, ...props }, ref) => {
+    return (
+      <div className="mb-4">
+        <label htmlFor={id} className="mb-1 block text-sm font-medium">
+          {label}
+        </label>
+        <input
+          id={id}
+          ref={ref}
+          {...props}
+          className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
+        />
+        {error ?? <div className="font-xs text-tomato">{error}</div>}
+      </div>
+    );
+  },
+);
+
+Input.displayName = "Input";
 
 export default Input;
--- src/components/form/text-area.tsx
+++ src/components/form/text-area.tsx
@@ -1,19 +1,29 @@
+import { forwardRef } from "react";
+
 type TextAreaProps = {
   label: string;
+  error: string | undefined;
 } & React.ComponentPropsWithRef<"textarea">;
 
-function TextArea({ label, ...props }: TextAreaProps) {
-  return (
-    <div className="mb-4">
-      <label htmlFor="note" className="mb-1 block text-sm font-medium">
-        {label}
-      </label>
-      <textarea
-        {...props}
-        className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
-      />
-    </div>
-  );
-}
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ id, label, error, ...props }, ref) => {
+    return (
+      <div className="mb-4">
+        <label htmlFor={id} className="mb-1 block text-sm font-medium">
+          {label}
+        </label>
+        <textarea
+          id={id}
+          ref={ref}
+          {...props}
+          className="w-full rounded-xl border-0 px-4 py-5 text-sm font-medium ring-2 ring-gray-300 placeholder:text-sm placeholder:font-medium placeholder:text-gray-300 focus:ring-2 focus:ring-primary"
+        />
+        {error ?? <div className="font-xs text-tomato">{error}</div>}
+      </div>
+    );
+  },
+);
+
+TextArea.displayName = "TextArea";
 
 export default TextArea;
