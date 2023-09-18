22dbc7c75da0eb89080ec9708cc779314ea2f6ed
Ridho Febriansa <officialridhof@gmail.com>
2023-09-18 07:27:30 +0000

    refactor: use API BASE URL from env

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,12 +1,11 @@
 import useSWR, { Fetcher } from "swr";
 
-import { API_BASE_URL } from "./static";
 import { SearchCategoryResponse } from "~/types/api/category";
 
 function fetchCategorySearch(query: string) {
     const fetcher: Fetcher<SearchCategoryResponse, string> = (url: string) => fetch(url).then((res) => res.json());
     const { data, error, isLoading } = useSWR(
-        `${API_BASE_URL}/kategori/search?namaKategori=${query}`,
+        `${process.env.API_BASE_URL}/kategori/search?namaKategori=${query}`,
         fetcher,
     );
     return {
--- src/api/static.ts
+++
@@ -1,3 +0,0 @@
-const API_BASE_URL = "https://pleasant-plum-pinafore.cyclic.cloud/api";
-
-export { API_BASE_URL };
